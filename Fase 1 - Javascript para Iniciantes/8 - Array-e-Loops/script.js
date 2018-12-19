5eebe66cc47d39b5bc9a58fcb23d79b3cdbb4646
Saullo SAM <saullo.sam115@gmail.com>
2018-12-19 10:58:52 -0200

    Array e Loop

--- Fase 1 - Javascript para Iniciantes/8 - Array-e-Loops/script.js
+++ Fase 1 - Javascript para Iniciantes/8 - Array-e-Loops/script.js
@@ -39,3 +39,60 @@ while (i < 10) {
     console.log(i);
     i++;
 }
+
+/**
+ * Array e Loop
+ */
+var $console = ['Switch', 'PS4', 'XBOX'];
+for (var i = 0; i < $console.length; i++) {
+    console.log($console[i]);
+}
+
+/**
+ * Break
+ * 
+ * O loop irá parar caso encontro a  palavra Break
+ */
+for (var i = 0; i < $console.length; i++) {
+    console.log($console[i]);
+
+    if ($console[i] === "PS4") { break; }
+}
+
+/**
+ * ForEach
+ * 
+ * 'forEach' é um método que executa uma função para cada item da array.
+ * É uma forma mais simples de utilizarmos um loop com array (ou array-like).
+ * 
+ * Podemos passar os seguintes parâmetros "item", "index" e "array".
+ */
+$console.forEach (function (item) {
+    console.log(item);
+});
+
+/**
+ * Exercício
+ */
+
+// Crie um array com os anos que o brasil ganhou a copa
+var copa = [1959, 1962, 1970, 1994, 2002];
+
+//Interaja com o array utilizando um loop para mostrar no console a seguinte mensagem.
+// `O brasil ganhou a copa de ${ano}`
+copa.forEach((ano) => {
+    console.log(`O brasil ganhou a copa de ${ano}`);
+});
+
+// Interaja com o loop nas frutas abaixo e para ao chegar na 'Pera'
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
+for (var i = 0; i < frutas.length; i++) {
+    if (frutas[i] === "Pera") { 
+        console.log(`Parou - ${frutas[i]}`); 
+        break;
+    }
+}
+
+// Coloque a última fruta do array acima em uma variável, sem remover a mesma do array; 
+var ultimaFruta = frutas[(frutas.length - 1)];
+console.log(`Ultima Fruta - ${ultimaFruta}`);
