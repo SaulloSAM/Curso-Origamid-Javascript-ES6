f6a362bffad6e29ea7149b5efcb878581963bad4
Saullo SAM <saullo.sam115@gmail.com>
2019-03-18 17:28:30 -0300

    Update app.js

--- Fase 3 - Objetos/4 - String/app.js
+++ Fase 3 - Objetos/4 - String/app.js
@@ -59,3 +59,166 @@ console.log(`Possui a fruta - ${fruta.includes(listaFrutas)}`);
 console.log(`endWith - ${fruta.endsWith('nana')}`);
 console.log(`startsWith - ${fruta.startsWith('Ba')}`);
 console.log(`startsWith - ${fruta.startsWith('na')}`);
+
+/**
+ * [str.slice(start,end)]
+ * 
+ * Corta a string de acordo com os valores de start e end.
+ */
+const transacao1 = "Depósito do cliente";
+const transacao2 = "Depósito do fornecedor";
+const transacao3 = "Taxa de camisas";
+
+console.log(`SLICE - T1 - ${transacao1.slice(0,3)}`);
+console.log(`SLICE - T2 - ${transacao2.slice(0,3)}`);
+console.log(`SLICE - T3 - ${transacao3.slice(0,3)}`);
+
+console.log(`SLICE - T1 - ${transacao1.slice(12)}`);
+console.log(`SLICE - T1 - ${transacao1.slice(-4)}`);
+console.log(`SLICE - T1 - ${transacao1.slice(3,6)}`);
+
+/**
+ * [str.substring(start,end)]
+ * 
+ * Corta a string de acordo com os valores de start e end. Não funciona com valores negativos como o slice.
+ */
+const linguagem3 = "JavaScript";
+
+console.log(`SUB - L3 - ${linguagem2.substring(3,5)}`);
+console.log(`SUB - L3 - ${linguagem2.substring(0,4)}`);
+console.log(`SUB - L3 - ${linguagem2.substring(4)}`);
+console.log(`SUB - L3 - ${linguagem2.substring(-3)}`);
+
+/**
+ * [str.indexOf(search)] e [str.lastIndexOf(search)]
+ * 
+ * Retorn o index da string, assim que achar o primeiro resultado ele já retorna.
+ * No caso do lastIndexOf ele retorna o último resultado.
+ */
+const instrumento = "Guitarra";
+
+console.log(`I - ${instrumento.indexOf("r")}`);
+console.log(`L - ${instrumento.lastIndexOf("r")}`);
+console.log(`I - ${instrumento.indexOf("ta")}`);
+
+/**
+ * [str.padstart(search)] e [str.padend(search)]
+ * 
+ * Aumenta o tamanho da string para o valor de n. Ou seja, uma string com 8 caracteres, se passarmos n = 10, ela passará a ter 10 caracteres. * O preenchimento é feito com espaços, caso não seja declarado o segundo argumento.
+ */
+const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+
+listaPrecos.forEach((preco) => {
+    console.log(preco.padStart(10, '.'));
+});
+
+console.log(`S - ${listaPrecos[0].padStart(10, '.')}`);
+console.log(`E - ${listaPrecos[0].padEnd(10, '.')}`);
+
+/**
+ * [str.repeat(n)]
+ * 
+ * Repete a string n vezes.
+ */
+const frase3 = "Ta";
+console.log(`Repeat - ${frase3.repeat(10)}`);
+
+/**
+ * [str.replace(regexp/substr,newstr/function)]
+ * 
+ * Troca parte da string por outra. Podemos utilizar uma regular expression ou um valor direto. Se usarmos um valor direto ele irá trocar
+ * apenas o primeiro valor que encontrar.
+ */
+let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+listaItens = listaItens.replace(/[ ]+/g, ', ');
+
+let preco = 'R$ 1200,43';
+preco = preco.replace(',', '.'); // 'R$ 1200.43'
+
+/**
+ * [str.split(padrao)]
+ * 
+ * Divide a string de acordo com o padrão passado e retorna uma array.
+ * join é um método de Array.
+ */
+const listaItens2 = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+const arrayItens = listaItens2.split(' ');
+
+const htmlText = '<div>O melhor item</div><div>A melhor lista</div>';
+const htmlArray = htmlText.split('div');
+const htmlNovo = htmlArray.join('section');
+
+/**
+ * [str.tolowercase()] e [str.touppercase()]
+ * 
+ * Retorna a string em letras maiúsculas ou minúsculas. Bom para verificarmos input de usuários.
+ */
+const sexo1 = 'Feminino';
+const sexo2 = 'feminino';
+const sexo3 = 'FEMININO';
+
+(sexo1.toLowerCase() === 'feminino'); // true
+(sexo2.toLowerCase() === 'feminino'); // true
+(sexo3.toLowerCase() === 'feminino'); // true
+
+/**
+ * [str.trim()] e [str.trimstrar()] e [str.trimend()]
+ * 
+ * Remove espaço em branco do início ou final de uma string.
+ */
+const valor = '  R$ 23.00   ' 
+valor.trim(); // 'R$ 23.00'
+valor.trimStart(); // 'R$ 23.00   '
+valor.trimEnd(); // '  R$ 23.00'
+
+console.log(`---------------------------------------------------------`);
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+const transacoes = [
+    {
+      descricao: 'Taxa do Pão',
+      valor: 'R$ 39',
+    },
+    {
+      descricao: 'Taxa do Mercado',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 99',
+    },
+    {
+      descricao: 'Taxa do Banco',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 49',
+    },
+  ];
+
+  let soma = 0;
+
+    transacoes.forEach(e => {
+        soma += parceInt(e.valor.replace("R$ ", ""));
+    });
+
+  console.log("Soma = " + soma);
+  
+  // Retorne uma array com a lista abaixo
+  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  
+  // Substitua todos os span's por a's
+  const html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
+  
+  // Retorne o último caracter da frase
+  const frase4 = 'Melhor do ano!';
+  console.log(frase4[frase4.length - 1]);
+  
+  // Retorne o total de taxas
+  const transacoes4 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
